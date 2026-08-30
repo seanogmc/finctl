0fe30b9d968978d6ef8729dc3eab294b70e241e6
Sean Óg McCartan <seanogmc@yahoo.com>
2026-08-30 18:24:37 +0100

    Add example config file for README

---
+++ js/config.example.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+  WORKER_ID: "https://your-worker.your-subdomain.workers.dev",
+  CALENDAR_ID: "your_calendar_id@group.calendar.google.com",
+};
